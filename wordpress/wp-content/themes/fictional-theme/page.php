41d00b6a994b51423706a681f99d834ed4878e3e
mpk.coding <mpk.coding@gmail.com>
2025-05-21 15:41:57 +0200

    Finished section 4 and 5.

---
+++ wordpress/wp-content/themes/fictional-theme/page.php
@@ -0,0 +1,71 @@
+<?php
+
+get_header();
+
+while (have_posts()) {
+  the_post(); ?>
+
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo the_title(); ?></h1>
+      <div class="page-banner__intro">
+        <p>Insert custom field here.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container container--narrow page-section">
+
+    <?php
+    //  for breadcrumbs
+    $parent = wp_get_post_parent_id(get_the_ID());
+    $parentUrl = get_permalink($parent);
+    $parentTitle = get_the_title($parent);
+
+    if ($parent) { ?>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo $parentUrl; ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo $parentTitle; ?></a> <span class="metabox__main"><?php echo the_title(); ?></span>
+        </p>
+      </div>
+    <?php }
+    ?>
+
+    <?php
+    $hasChildren = get_pages(array(
+      'child_of' => get_the_ID()
+    ));
+    if ($parent || $hasChildren) { ?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo $parentUrl; ?>"><?php echo $parentTitle; ?></a></h2>
+        <ul class="min-list">
+          <?php
+          // for list of child pages
+          if ($parent) {
+            $child_of = $parent;
+          } else {
+            $child_of = get_the_ID();
+          }
+
+          wp_list_pages(array(
+            'title_li' => NULL,
+            'child_of' => $child_of,
+            'sort_column' => 'menu_order'
+          ));
+          ?>
+          <!-- <li class="current_page_item"><a href="#">Our History</a></li>
+        <li><a href="#">Our Goals</a></li> -->
+        </ul>
+      </div>
+
+      <div class="generic-content">
+        <?php the_content(); ?>
+      </div>
+  </div>
+<?php } ?>
+<?php }
+
+get_footer();
+
+?>
