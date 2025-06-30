2da6ecd4917c52e7dafd0065a1ca956a73d4b59b
mpk.coding <mpk.coding@gmail.com>
2025-06-30 13:20:07 +0200

    30. Displaying Custom Post Types

--- wordpress/wp-content/mu-plugins/index.php
+++ wordpress/wp-content/mu-plugins/index.php
@@ -1,17 +1,23 @@
 <?php
 
+//  Registering custom post types
 function post_types()
 {
     // wp fn
     // post type name, array of props
     register_post_type('event', array(
+        'rewrite' => array(
+            'slug' => 'events'
+        ),
+        'has_archive' => true,
         'public' => true,
         'menu_icon' => 'dashicons-calendar',
+        'show_in_rest' => true, //  enable block editor
         'labels' => array(
             'name' => "Events",
             'add_new_item' => 'Add New Event',
             'all_items' => 'All events',
-            'singular_name' => 'Event'
+            'singular_name' => 'Event',
         )
     ));
 }
