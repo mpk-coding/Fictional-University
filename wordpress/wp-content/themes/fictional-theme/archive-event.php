17ef9c9c0f89d997b513bba0473fec127db1c2da
mpk.coding <mpk.coding@gmail.com>
2025-07-02 14:34:47 +0200

    35. Manipulating Default URL Based Queries

--- wordpress/wp-content/themes/fictional-theme/archive-event.php
+++ wordpress/wp-content/themes/fictional-theme/archive-event.php
@@ -24,8 +24,18 @@ get_header(); ?>
 
         <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
-                <span class="event-summary__month"><?php echo get_the_date('M'); ?></span>
-                <span class="event-summary__day"><?php echo get_the_date('j'); ?></span>
+                <span class="event-summary__month">
+                    <?php
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M');
+                    ?>
+                </span>
+                <span class="event-summary__day">
+                    <?php
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('d');
+                    ?>
+                </span>
             </a>
             <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
