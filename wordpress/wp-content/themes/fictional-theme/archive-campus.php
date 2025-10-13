efc500e751779cbc77ce256f013dd2ff59875f92
mpk.coding <mpk.coding@gmail.com>
2025-10-13 12:32:47 +0200

    54, 55. Campus Post Type, 56. Campus Map on Front-End

---
+++ wordpress/wp-content/themes/fictional-theme/archive-campus.php
@@ -0,0 +1,31 @@
+<?php
+
+get_header();
+pageBanner(array(
+    'title' => 'Our Campuses',
+    'subtitle' => 'All of our campuses'
+)); ?>
+
+<div class="container container--narrow page-section">
+    <div class='acf-map'>
+        <?php
+        //  start the loop
+        while (have_posts()) {
+            //  setup global data for post
+            the_post();
+            $mapLocation = get_field('map_location');
+        ?>
+            <div
+                class='marker'
+                data-lat='<?php echo $mapLocation['lat'] ?>'
+                data-lng='<?php echo $mapLocation['lng'] ?>'>
+            </div>
+
+        <?php
+        }
+        //  add pagination
+        echo paginate_links();
+        ?>
+    </div>
+    <?php
+    get_footer();
