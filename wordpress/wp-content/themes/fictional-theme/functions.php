fde4ec03ed46773a66477db3241e211b195c74ef
mpk.coding <mpk.coding@gmail.com>
2025-10-21 19:09:56 +0200

    Fixed leaked gmaps key

--- wordpress/wp-content/themes/fictional-theme/functions.php
+++ wordpress/wp-content/themes/fictional-theme/functions.php
@@ -43,7 +43,7 @@ function pageBanner($args = [])
 
 function university_files()
 {
-  wp_enqueue_script('google-maps', '//maps.googleapis.com/maps/api/js?key=AIzaSyATEMb3Ocu50gMD9Fj7RrQl4fG5NVru6wU', NULL, '1.0', true);
+  wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?key=' . GOOGLE_MAPS_API_KEY, NULL, '1.0', true);
   wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -56,7 +56,9 @@ function university_files()
     'root_url' => get_site_url()
   ));
 }
-
+add_action('wp_footer', function () {
+  echo '<script>console.log("Google Maps key:", "' . GOOGLE_MAPS_API_KEY . '");</script>';
+});
 add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features()
