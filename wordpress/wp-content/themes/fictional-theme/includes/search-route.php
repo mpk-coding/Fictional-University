86856cbf7ae27d55861d794ba50d1fec70a18883
mpk.coding <mpk.coding@gmail.com>
2025-10-24 12:21:07 +0200

    77. Working with multiple post types.

--- wordpress/wp-content/themes/fictional-theme/includes/search-route.php
+++ wordpress/wp-content/themes/fictional-theme/includes/search-route.php
@@ -12,27 +12,80 @@ add_action('rest_api_init', "universityRegisterSearch");
 
 function universitySearchResults($data)
 {
-    // get data
-    $professors = new WP_Query(array(
-        'post_type' => 'professor',
+    // get all data
+    $mainQuery = new WP_Query(array(
+        'post_type' => array(
+            'post',
+            'page',
+            'professor',
+            'campus',
+            'event',
+            'programs'
+        ),
         'posts_per_page' => -1,
         's' => sanitize_text_field($data['term'])
     ));
 
-
-    $professorResults = array();
+    // build response object
+    $mainQueryResults = array(
+        'generalInfo' => array(),
+        'professors' => array(),
+        'programs' => array(),
+        'events' => array(),
+        'campuses' => array()
+    );
 
     // loop through data
-    while ($professors->have_posts()) {
-        $professors->the_post();
-        // populate custom data array
-        array_push($professorResults, array(
-            'id' => get_the_ID(),
-            'title' => get_the_title(),
-            'permalink' => get_the_permalink()
+    while ($mainQuery->have_posts()) {
+        $mainQuery->the_post();
 
-        ));
+        // populate custom data object
+        if (get_post_type() == 'post' || get_post_type() == 'page') {
+            array_push($mainQueryResults['generalInfo'], array(
+                'id' => get_the_ID(),
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+
+            ));
+        }
+
+        // cases
+        if (get_post_type() == 'professor') {
+            array_push($mainQueryResults['professors'], array(
+                'id' => get_the_ID(),
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+
+            ));
+        }
+
+        if (get_post_type() == 'program') {
+            array_push($mainQueryResults['programs'], array(
+                'id' => get_the_ID(),
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+
+            ));
+        }
+
+        if (get_post_type() == 'campus') {
+            array_push($mainQueryResults['campuses'], array(
+                'id' => get_the_ID(),
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+
+            ));
+        }
+
+        if (get_post_type() == 'event') {
+            array_push($mainQueryResults['events'], array(
+                'id' => get_the_ID(),
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+
+            ));
+        }
     }
 
-    return $professorResults;
+    return $mainQueryResults;
 }
