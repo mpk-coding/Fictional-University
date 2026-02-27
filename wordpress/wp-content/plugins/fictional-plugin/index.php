0b9c26efd8edb6fb711b69f92897fc60aafcdf75
mpk.coding <mpk.coding@gmail.com>
2026-02-27 16:01:29 +0100

    126. Settings API (Saving Settings Data)

--- wordpress/wp-content/plugins/fictional-plugin/index.php
+++ wordpress/wp-content/plugins/fictional-plugin/index.php
@@ -17,6 +17,41 @@ class WordCountAndTimePlugin
             $this,
             'admin_page'
         ));
+
+        add_action('admin_init', array(
+            $this,
+            'settings'
+        ));
+    }
+
+    function settings()
+    {
+        add_settings_section(
+            'wcp_first_section',
+            null,
+            null,
+            'word-count-settings'
+        );
+
+        add_settings_field(
+            'wcp_location',
+            'Display location',
+            array(
+                $this,
+                'location_html'
+            ),
+            'word-count-settings',
+            'wcp_first_section'
+        );
+
+        register_setting(
+            'WordCountPlugin',
+            'wcp_location',
+            array(
+                'sanitize_callback' => 'sanitize_text_field',
+                'default' => 0
+            )
+        );
     }
 
     function admin_page()
@@ -37,7 +72,22 @@ class WordCountAndTimePlugin
     { ?>
         <div class='wrapper'>
             <h1>Word Count Settings</h1>
+            <form action="options.php" method="POST">
+                <?php
+                settings_fields('WordCountPlugin');
+                do_settings_sections('word-count-settings');
+                submit_button();
+                ?>
+            </form>
         </div>
+    <?php }
+
+    function location_html()
+    { ?>
+        <select name="wcp_location">
+            <option value="0">Beginning of Post</option>
+            <option value="1">End of Post</option>
+        </select>
 <?php }
 }
 
